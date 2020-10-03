878ebf34de2c6b9eec4e4a43438a24140e678291
omprakashch3 <omprakashchautala3@gmail.com>
2020-10-03 11:28:26 +0530

    JSON & nodemon Basics

--- first_server/index.js
+++ first_server/index.js
@@ -4,6 +4,18 @@ const http = require("http");
 const PORT = process.env.PORT || 4000;
 
 const server = http.createServer((req, res) => {
+  const { url } = req;
+  console.log(url);
+  if (url === "/translations") {
+    const translations = { 1: "one", 2: "two", 3: "three" };
+
+    res.setHeader("Content-Type", "application/json");
+    res.write(JSON.stringify(translations));
+    res.end();
+
+    // res.end("Translations");
+  }
+
   res.end("hello Om Gm");
 });
 
