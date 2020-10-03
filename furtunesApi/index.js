215e848250bec8553c2695a7d93369eec4ff5a8f
omprakashch3 <omprakashchautala3@gmail.com>
2020-10-03 13:47:45 +0530

    getting a random fortune data or a data using id

--- furtunesApi/index.js
+++ furtunesApi/index.js
@@ -2,10 +2,23 @@ const express = require("express");
 const fortunes = require("./data/fortune.json");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/fortunes", (req, res) => {
   res.json(fortunes);
+});
+
+app.get("/fortunes/random", (req, res) => {
+  // // res.send("getting a random fortunes...");
+  // const randomIndex = Math.floor(Math.random() * fortunes.length);
+
+  // const rFortunes = fortunes[randomIndex];
+  res.send(fortunes[Math.floor(Math.random() * fortunes.length)]);
+});
+
+app.get("/fortunes/:id", (req, res) => {
+  // console.log(req.params);
+  res.json(fortunes.find((f) => f.id == req.params.id));
 });
 
 app.listen(PORT, () => {
